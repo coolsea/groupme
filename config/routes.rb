b5530d2b1f90f78a54d9bbc8ffe850b65840fd46
coolsea <wei_918@coolsea.net>
2014-01-21 17:30:15 +0800

    finish posts ch2 尚未完成整理code

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Groupme::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => 'high_voltage/pages#show', :id => 'welcome'
 
-  resources :groups
+  resources :groups do
+  	resources :posts
+  end
 
 end
